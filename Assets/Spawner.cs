a8f936b62396ad87c6896c44c94c8eaf824e3bcf
AbbeyBricks <aroo3222@gmail.com>
2020-04-10 00:09:53 -0400

    Tutorial
    Tutorial

---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject monster;
+    public float startTimeBtwMonster;
+    private float timeBtwMonster;
+
+    public int numberOfMonsters;
+
+    private void Update()
+    {
+        if(timeBtwMonster <= 0 && numberOfMonsters > 0)
+        {
+            Instantiate(monster, transform.position, Quaternion.identity);
+            timeBtwMonster = startTimeBtwMonster;
+            numberOfMonsters--;
+        }
+        else
+        {
+            timeBtwMonster -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+    public GameObject spawner;
+    public float waitTime = 2f;
+    private void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+        if (popUpIndex == 0)
+        {
+            if (Input.touchSupported)
+            {
+                popUpIndex++;
+            }
+            else if (popUpIndex == 1)
+            {
+                if (Input.touchSupported)
+                {
+                    popUpIndex++;
+                }
+                else if (popUpIndex == 2)
+                {
+                    if(waitTime <= 0)
+                    {
+                       spawner.SetActive(true);
+                    }
+                    else
+                    {
+                        waitTime -= Time.deltaTime;
+                    }
+                    
+                }
+               
+            }
+        }
+    }
+}
