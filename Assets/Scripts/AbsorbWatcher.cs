01faf4b4a8cabf7b5b8afc00ab1501d2581e5ecf
AbbeyBricks <aroo3222@gmail.com>
2020-04-16 22:48:15 -0400

    update
    update

---
+++ Assets/Scripts/AbsorbWatcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbsorbWatcher : MonoBehaviour
+{
+    public GameObject[] absorbs;
+    public bool popcorn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        absorbs = GameObject.FindGameObjectsWithTag("absorb");
+
+        if (popcorn == true)
+        {
+            for (int i = 0; i < absorbs.Length; i++)
+            {
+                absorbs[i].GetComponent<GotHit>().popcorn = true;
+            }
+        }
+        else
+        {
+
+        }
+    }
+}
